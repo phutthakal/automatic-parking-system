b0bb7606eb97951ce974ddd38cf550f038dbcbfe
Aphiwat Chalongtham <138902148+Aphiwatch@users.noreply.github.com>
2023-07-08 01:37:27 +0700

    Update models.py

--- APS_fast_api_by_aphiwat/models.py
+++ APS_fast_api_by_aphiwat/models.py
@@ -3,6 +3,6 @@ from uuid import UUID, uuid4
 from pydantic import BaseModel
 
 class User(BaseModel):
-    # id: Optional[UUID] = uuid4
+    id: Optional[UUID] = uuid4()
     license_plate: str
     height: int
