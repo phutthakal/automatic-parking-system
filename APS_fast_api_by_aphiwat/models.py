88f9e129898311bc41dbdf13938ac82a5437e30f
Aphiwatch <138902148+Aphiwatch@users.noreply.github.com>
2023-07-07 23:54:03 +0700

    Add files via upload

---
+++ APS_fast_api_by_aphiwat/models.py
@@ -0,0 +1,8 @@
+from typing import Optional
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+
+class User(BaseModel):
+    # id: Optional[UUID] = uuid4
+    license_plate: str
+    height: int
